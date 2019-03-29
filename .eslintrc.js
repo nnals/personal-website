223bb1cce28da821d6a1225b04068ed35d40b0ad
Nicolai Benker <n@benker.de>
2019-03-29 21:19:45 +0100

    Fix eslint dependencies and config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,11 @@ module.exports = {
   extends: ['airbnb', 'react-app', 'prettier', 'plugin:jest/recommended'],
   rules: {
     'prettier/prettier': 'error',
+    'react/jsx-filename-extension': 0,
+    'react/prop-types': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'react/jsx-curly-brace-presence': 0,
+    'jsx-a11y/label-has-for': 0, // deprecated
+    'jsx-a11y/label-has-associated-control': 0,
   },
 }
